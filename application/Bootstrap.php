451f96c86140b2b1eabee6d8780424aa569b5b34
igoryen <igor.e@advertiseit.com>
2015-05-28 18:25:17 -0400

    P33) Change doctype using a standard view helper

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -5,7 +5,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {
   protected function _initDoctype() {
     $this->bootstrap('view');
     $view = $this->getResource('view');
-    $view->doctype('XHTML1_STRICT');
+    $view->doctype('HTML5');
   }
   
   protected function _initSidebar() {
