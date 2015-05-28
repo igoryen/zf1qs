4563c994b6caccd7d27632dd8cef208ab69fcbc4
igoryen <igor.e@advertiseit.com>
2015-05-28 12:56:32 -0400

    19) Created GuestbookMapper.php

--- application/models/DbTable/Guestbook.php
+++ application/models/DbTable/Guestbook.php
@@ -1,10 +1,7 @@
 <?php
 
-class Application_Model_DbTable_Guestbook extends Zend_Db_Table_Abstract
-{
+class Application_Model_DbTable_Guestbook extends Zend_Db_Table_Abstract {
 
-    protected $_name = 'guestbook';
-
+  protected $_name = 'guestbook';
 
 }
-
