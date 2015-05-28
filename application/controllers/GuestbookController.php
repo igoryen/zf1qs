44f91c7d925d79ea331fc3bb8706a1d5d5472bd4
igoryen <igor.e@advertiseit.com>
2015-05-28 13:08:30 -0400

    -- ignore: formatting

--- application/controllers/GuestbookController.php
+++ application/controllers/GuestbookController.php
@@ -1,18 +1,13 @@
 <?php
 
-class GuestbookController extends Zend_Controller_Action
-{
+class GuestbookController extends Zend_Controller_Action {
 
-    public function init()
-    {
-        /* Initialize action controller here */
-    }
+  public function init() {
+    /* Initialize action controller here */
+  }
 
-    public function indexAction()
-    {
-        // action body
-    }
-
+  public function indexAction() {
+    // action body
+  }
 
 }
-
