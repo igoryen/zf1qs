c56cb1dcc199d8f0ee195dec67ae7ee73a8def3f
igoryen <igor.e@advertiseit.com>
2015-05-28 13:04:13 -0400

    21) created Model Guestbook

--- application/models/Guestbook.php
+++ application/models/Guestbook.php
@@ -1,7 +1,5 @@
 <?php
 
-// application/models/Guestbook.php
-
 class Application_Model_Guestbook {
 
   protected $_comment;
@@ -9,32 +7,73 @@ class Application_Model_Guestbook {
   protected $_email;
   protected $_id;
 
-  public function __set($name, $value);
+  public function __construct(array $options = null) {
+    if (is_array($options)) {
+      $this->setOptions($options);
+    }
+  }
 
-  public function __get($name);
+  public function __set($name, $value) {
+    $method = 'set' . $name;
+    if (('mapper' == $name) || !method_exists($this, $method)) {
+      throw new Exception('Invalid guestbook property');
+    }
+    $this->$method($value);
+  }
 
-  public function setComment($text);
+  public function __get($name) {
+    $method = 'get' . $name;
+    if (('mapper' == $name) || !method_exists($this, $method)) {
+      throw new Exception('Invalid guestbook property');
+    }
+    return $this->$method();
+  }
 
-  public function getComment();
+  public function setOptions(array $options) {
+    $methods = get_class_methods($this);
+    foreach ($options as $key => $value) {
+      $method = 'set' . ucfirst($key);
+      if (in_array($method, $methods)) {
+        $this->$method($value);
+      }
+    }
+    return $this;
+  }
 
-  public function setEmail($email);
+  public function setComment($text) {
+    $this->_comment = (string) $text;
+    return $this;
+  }
 
-  public function getEmail();
+  public function getComment() {
+    return $this->_comment;
+  }
 
-  public function setCreated($ts);
+  public function setEmail($email) {
+    $this->_email = (string) $email;
+    return $this;
+  }
 
-  public function getCreated();
+  public function getEmail() {
+    return $this->_email;
+  }
 
-  public function setId($id);
+  public function setCreated($ts) {
+    $this->_created = $ts;
+    return $this;
+  }
 
-  public function getId();
-}
+  public function getCreated() {
+    return $this->_created;
+  }
 
-class Application_Model_GuestbookMapper {
+  public function setId($id) {
+    $this->_id = (int) $id;
+    return $this;
+  }
 
-  public function save(Application_Model_Guestbook $guestbook);
+  public function getId() {
+    return $this->_id;
+  }
 
-  public function find($id);
-
-  public function fetchAll();
 }
